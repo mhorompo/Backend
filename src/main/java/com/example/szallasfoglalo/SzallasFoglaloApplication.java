3c3e113d9812dce726d25d13a5252f38eba78d4f
h159013 <h159013@stud.u-szeged.hu>
2023-10-07 16:12:36 +0200

    Projekt kezdete

---
+++ src/main/java/com/example/szallasfoglalo/SzallasFoglaloApplication.java
@@ -0,0 +1,13 @@
+package com.example.szallasfoglalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzallasFoglaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzallasFoglaloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/szallasfoglalo/SzallasFoglaloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.szallasfoglalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzallasFoglaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
